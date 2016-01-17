e4511fb5233614e5f2355abcdaa5cb5d81c4f2dd
Jade Shan <evokeralucard@gmail.com>
2016-01-17 14:12:18 +0800

    update

--- scripts/portal.js
+++ scripts/portal.js
@@ -52,6 +52,15 @@ var net = net || {};
 
 	self.renderSubTitle = function (page) { $("#subTitle").html(page.subTitle); };
 
+	self.renderPicItem = function (itm) {
+		var html = '<div class="col-sm-6 col-md-3"><div class="thumbnail">';
+		html = html + '<img id="' + itm.id + '" src="' + itm.url +'" alt="' + 
+			itm.title +'"></div>';
+		html = html + '<div class="caption"><h3>' + itm.title + '</h3><p>' + 
+			itm.desc + '</p></div></div>';
+		return html;
+	};
+
 	self.renderArticle = function (itm) {
 		var html = '<div class="item">';
 		html = html + '<div class="title">' + itm.title + '</div>';
@@ -60,7 +69,15 @@ var net = net || {};
 		html = html + '<div class="metadata">' + t.toLocaleString() +
 			' by ' + itm.auth + '</div>';
 		html = html + '<div class="body">' + 
-			net.jadedungeon.markdown.makeHtml(itm.text) + '</div></div>';
+			net.jadedungeon.markdown.makeHtml(itm.text) + '</div>';
+		html = html + '</div>';
+		if (itm.ablum && itm.ablum.length > 0) {
+			html = html + '<div class="row">';
+			$.each(itm.ablum, function (i, pic) {
+				html = html + self.renderPicItem(pic);
+			});
+			html = html + '</div>';
+		}
 		html = html + '<div class="divider"><span></span></div>';
 		return html;
 	};
@@ -78,8 +95,16 @@ var net = net || {};
 	self.render = function () {
 		net.jadedungeon.renderTopNav(self.initCfg);
 		net.jadedungeon.renderSubTitle(self.initCfg);
+		self.renderGallery(self.initCfg.articles);
 	};
 
+	self.renderGallery = function (articles) {
+		var html = '<div class="spacer"></div>';
+		$.each(articles, function (i, itm) {
+			html = html + net.jadedungeon.renderArticle(itm);
+		});
+		$("#articles").html(html);
+	};
 })(jQuery);
 
 
