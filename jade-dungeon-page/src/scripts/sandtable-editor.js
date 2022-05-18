de6f77e64e6ddda20bbbecee40e41463acdfb359
Jade Shan <evokeralucard@gmail.com>
2022-05-18 15:31:25 +0800

    update

--- jade-dungeon-page/src/scripts/sandtable-editor.js
+++ jade-dungeon-page/src/scripts/sandtable-editor.js
@@ -1,1 +1,15 @@
 /* jshint esversion: 8 */
+class SandTableEditor {
+
+	constructor(canvas, scene, userId) {
+		this.canvas    = canvas;
+		this.ctx       = canvas.getContext("2d");
+		this.userId    = userId;
+		this.observer  = {};
+		this.scene     = { width: 0, height: 0, 
+			campaignId: scene.campaignId, placeId: scene.placeId, sceneId: scene.sceneId,
+			shadowColor: scene.shadowColor, viewRange: scene.viewRange,
+			creaters : [], teams: [], walls: [], doors: [], furnishing: [], images:{}};
+	}
+
+}
