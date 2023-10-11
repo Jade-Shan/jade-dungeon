495bdeacb7f052c4bac125a678df7050a1f9a4b3
Jade Shan <evokeralucard@gmail.com>
2023-10-11 17:45:26 +0800

    update

--- jade-dungeon-page-v2/src/scripts/components/sandtable-view.tsx
+++ jade-dungeon-page-v2/src/scripts/components/sandtable-view.tsx
@@ -1,6 +1,7 @@
 import {defaultImgData, loadDefaultImage} from '../utils/defaultImages'
 import { Canvas2dShape, CanvasCircle, ImageInfo, Observer } from '../utils/canvasGeo';
 import * as STCom from './sandtable-common'; // Sandtable common
+import { Point2D } from '../utils/geo2d';
 
 
 export let initSandtable = async (cvs: HTMLCanvasElement, cvsCtx: CanvasRenderingContext2D): Promise<void> => {
@@ -9,7 +10,7 @@ export let initSandtable = async (cvs: HTMLCanvasElement, cvsCtx: CanvasRenderin
 	await STCom.drawSence(cvs, cvsCtx, scene, observer);
 };
 
-let findObserverOnMap = async (scene: STCom.Scence, userId: string) => {
+let findObserverOnMap = async (scene: STCom.Scence, userId: string): Promise<Observer> => {
 	let obtm: Canvas2dShape = new CanvasCircle('spectator', { x: 10, y: 10 }, 50, 
 		scene.shadowColor, await loadDefaultImage(), false, false);
 	if (scene.teams) {
@@ -27,3 +28,17 @@ let findObserverOnMap = async (scene: STCom.Scence, userId: string) => {
 	return new Observer("obs", obtm.location.x, obtm.location.y, scene.viewRange, obtm);
 }
 
+let moveObs = (observer: Observer, dx: number, dy: number) => {
+	observer.location.x  += dx;
+	observer.location.y  += dy;
+	observer.body.location.x += dx;
+	observer.body.location.y += dy;
+}
+
+function caculateNodeOffset (node: HTMLElement, location: Point2D): Point2D {
+	location.x += node.offsetLeft - node.scrollLeft;
+	location.y += node.offsetTop  - node.scrollTop;
+
+	let parent: HTMLElement  = node.offsetParent as HTMLElement;
+	return parent ? caculateNodeOffset(parent, location) : location;
+}
