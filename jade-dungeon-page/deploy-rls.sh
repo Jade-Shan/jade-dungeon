cda5bcc1585ae2b3044561504326396fea9e287e
Jade Shan <evokeralucard@gmail.com>
2025-02-07 18:32:33 +0800

    update

--- jade-dungeon-page/deploy-rls.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#  node ./node_modules/gulp-cli/bin/gulp.js         
-node ./node_modules/gulp-cli/bin/gulp.js release
-tar -cvf webroot.tar.gz webroot
-# scp webroot.tar.gz morgan@www.jade-dungeon.net:/home/morgan/workspace/nginx-instance/www.jade-dungeon.net/
-scp webroot.tar.gz   jade@www.jade-dungeon.net:/home/nginx/jadedungeon
